a56cc002c94156f06f1810cf2847357f4b8b584e
Vladimir <shmalko89@list.ru>
2021-11-15 21:50:07 +0400

    add homework7

---
+++ oop7/BCoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop7
+{
+    public class BCoder : ICoder
+    {
+        public string Decode(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encoder(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ oop7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace oop7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txt = "привет";
+            Console.WriteLine(txt);
+            var coder = new ACoder();
+            var codeText = coder.Encoder(txt);
+            Console.WriteLine(codeText);
+            var decodeText = coder.Decode(codeText);
+            Console.WriteLine(decodeText);
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
